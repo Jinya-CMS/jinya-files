b445cbc6eebcda41bd1cdf6a9e114836966774e3
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-05-25 23:00:12 +0200

    Added version scanner for the jinya cms

---
+++ src/Commands/UpdateVersionsJsonCommand.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanuel
+ * Date: 25.05.18
+ * Time: 21:53
+ */
+
+namespace Jinya\Commands;
+
+use Jinya\Services\Cms\VersionScannerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateVersionsJsonCommand extends ContainerAwareCommand
+{
+    private const PRODUCT_CMS = 'cms';
+
+    /** @var VersionScannerInterface */
+    private $versionScanner;
+    /** @var LoggerInterface */
+    private $logger;
+
+    /**
+     * UpdateVersionsJsonCommand constructor.
+     * @param VersionScannerInterface $versionScanner
+     * @param LoggerInterface $logger
+     */
+    public function __construct(VersionScannerInterface $versionScanner, LoggerInterface $logger)
+    {
+        parent::__construct();
+        $this->versionScanner = $versionScanner;
+        $this->logger = $logger;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('jinya:version:update')
+            ->addArgument('product', InputArgument::OPTIONAL, sprintf('Which product to you want to update? Currently available %s', self::PRODUCT_CMS), self::PRODUCT_CMS)
+            ->addArgument('mode', InputArgument::OPTIONAL, sprintf('Which versions should be updated, possible values are %s, %s and %s', VersionScannerInterface::VERSION_STABLE, VersionScannerInterface::VERSION_NIGHTLY, VersionScannerInterface::VERSION_ALL), VersionScannerInterface::VERSION_ALL);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $mode = $input->getArgument('mode');
+        if (!in_array($mode, [VersionScannerInterface::VERSION_ALL, VersionScannerInterface::VERSION_STABLE, VersionScannerInterface::VERSION_NIGHTLY])) {
+            $this->logger->error("The provided mode $mode is invalid");
+        } else {
+            $this->versionScanner->updateVersions($mode);
+        }
+    }
+}
